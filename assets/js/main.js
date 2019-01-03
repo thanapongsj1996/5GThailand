4158e9b6766c894d1a416e23a714c043110c88f6
Thanapong <thanapongsj1996@gmail.com>
2019-01-03 23:02:05 +0700

    Add blog-detail.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v3.2';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 $(window).scroll(function(){
     var scrollTop = $(this).scrollTop();
     if (scrollTop > 1) {
@@ -6,3 +14,21 @@ $(window).scroll(function(){
         $('#navbar').css('padding', '10px')
     }
 })
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+  });
