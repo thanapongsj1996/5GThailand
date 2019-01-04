ce9e4ddf784d4859027731f56651090d5470e3be
Thanapong <thanapongsj1996@gmail.com>
2019-01-04 21:59:35 +0700

    fixed, recaptcha, on to top, fix CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,20 +15,6 @@ $(window).scroll(function(){
     }
 })
 
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        nav:false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
-            }
-        }
-    });
-  });
+$('.to-top').click(function(){
+    $('html, body').animate({scrollTop: '0px'}, 800 )
+})
