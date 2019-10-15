e31a81559a306958eb052d06d8d70769d9d56e58
tanawatgoodboy <tanawatgood@gmail.com>
2019-10-15 15:20:42 +0700

    debugการลิ้งค์ไปหน้า use cases

--- include/navbar.php
+++ include/navbar.php
@@ -19,8 +19,8 @@
                     <li class="nav-item <?php echo $file_name == 'about' ? 'active':''  ?>">
                         <a class="nav-link" href="about.php">Road to 5G &ensp;|</a>
                     </li>
-                    <li class="nav-item <?php echo $file_name == 'Use case' ? 'active':''  ?>">
-                        <a class="nav-link" href="Use case.php">Use cases &ensp;|</a>
+                    <li class="nav-item <?php echo $file_name == 'Uses case' ? 'active':''  ?>">
+                        <a class="nav-link" href="Uses case.php">Use cases &ensp;|</a>
                     </li>
                     <li class="nav-item <?php echo $file_name == 'blog'|| $file_name == 'blog-detail' ? 'active':''  ?>">
                         <a class="nav-link" href="blog.php">Blog &ensp;|</a>
