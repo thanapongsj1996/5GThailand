88a693b60b03d92c77ce003f8bef9768d43d0fc2
tanawatgoodboy <tanawatgood@gmail.com>
2019-10-21 12:07:53 +0700

    ลองใส่section link ใน head และปรับข้อความ1บรรทัด

--- key-technologies.php
+++ key-technologies.php
@@ -24,6 +24,13 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Prompt">
     <link rel="stylesheet" href="assets/css/style.css">
 
+    <!--link-->
+    <style>
+      body {link='black'; vlink='blue' ; alink='green'}
+    </style>
+    
+
+
 </head>
 
 <body>
@@ -112,7 +119,7 @@
         <div class="col mb-3">
           <h5 class="ml-5 mb-3">TEST1</h5>
           <ul>
-            <li class="mt-2">boyboyboydklnglsa;dfnvalksnf;kl</li>
+            <a class="mt-2">boyboyboydklnglsa;dfnvalksnf;kl</a>
             <li class="mt-2">boyboyboydklnglsa;dfnvalksnf;kl</li>
             <li class="mt-2">boyboyboydklnglsa;dfnvalksnf;kl</li>
             <li class="mt-2">boyboyboydklnglsa;dfnvalksnf;kl</li>
@@ -122,6 +129,7 @@
         <div class="col mb-3">
           <h5 class="ml-5 mb-3">TEST2</h5>
           <ul>
+            <a href=''>คลิกสิ๊</a>
             <li class="mt-2">boyboyboydklnglsa;dfnvalksnf;kl</li>
             <li class="mt-2">boyboyboydklnglsa;dfnvalksnf;kl</li>
             <li class="mt-2">boyboyboydklnglsa;dfnvalksnf;kl</li>
